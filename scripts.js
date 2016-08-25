a4ecbda386d3008effd14e89e2ac10aae20d9b18
yoguido77 <yoguido77@gmail.com>
2016-08-25 18:33:52 -0500

    created shout program

---
+++ scripts.js
@@ -0,0 +1,11 @@
+//var passphrase = 'Open Sesame';
+//console.log(passphrase.length);
+//console.log(passphrase.toLowerCase());
+//console.log(passphrase);
+//console.log(passphrase.toUpperCase());
+
+//Shout Program
+var stringToShout = prompt("What should I shout?");
+var shout = stringToShout.toUpperCase();
+shout += "!!!";
+alert(shout);
