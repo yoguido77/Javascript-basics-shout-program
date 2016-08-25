1d855980e846bbdaa5919aae6e6a21e8f66c5db4
yoguido77 <yoguido77@gmail.com>
2016-08-25 18:40:03 -0500

    added code challenge

--- scripts.js
+++ scripts.js
@@ -9,3 +9,19 @@ var stringToShout = prompt("What should I shout?");
 var shout = stringToShout.toUpperCase();
 shout += "!!!";
 alert(shout);
+
+//Code Challenge Part 1
+//Assign an all uppercase version of the id variable to the //userName variable.
+
+var id = "23188xtr";
+var lastName = "Smith";
+
+var userName = id.toUpperCase();
+
+//Code Challenge Part 2
+//Complete the assignment to the userName variable by adding a # //symbol followed by an all uppercase version of the lastName //variable. In other words, using string concatenation so that the //final value of userName is "23188XTR#SMITH".
+
+var id = "23188xtr";
+var lastName = "Smith";
+
+var userName = id.toUpperCase() + "#" + lastName.toUpperCase();
